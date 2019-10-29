ad0f1d5b5294684751c196a656c8a95f0281efb2
Your Name <rtkard90@gmail.com>
2019-10-30 00:25:23 +0200

    pret fix

--- goit-js-hw-03/js/task-6.js
+++ goit-js-hw-03/js/task-6.js
@@ -1,41 +1,37 @@
 const products = [{
         name: 'Радар',
         price: 1300,
-        quantity: 4
+        quantity: 4,
     },
     {
         name: 'Сканер',
         price: 2700,
-        quantity: 3
+        quantity: 3,
     },
     {
         name: 'Дроид',
         price: 400,
-        quantity: 7
+        quantity: 7,
     },
     {
         name: 'Захват',
         price: 1200,
-        quantity: 2
+        quantity: 2,
     },
 ];
 
 function calculateTotalPrice(arr, productName) {
-
     let totalPrice = 0;
 
     for (let obj of arr) {
         Object.values(obj);
 
         if (Object.values(obj).includes(productName)) {
-
             totalPrice = obj['price'] * obj['quantity'];
-
         }
     }
 
-    return totalPrice
-
+    return totalPrice;
 }
 
 console.log(calculateTotalPrice(products, 'Радар')); // 5200
