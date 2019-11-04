14bd1d87a88178889ab575baa85efa010cc1b356
Your Name <rtkard90@gmail.com>
2019-11-05 01:53:41 +0200

    add hw4

--- goit-js-hw-03/js/task-6.js
+++ goit-js-hw-03/js/task-6.js
@@ -1,39 +1,40 @@
-const products = [{
-        name: 'Радар',
-        price: 1300,
-        quantity: 4,
-    },
-    {
-        name: 'Сканер',
-        price: 2700,
-        quantity: 3,
-    },
-    {
-        name: 'Дроид',
-        price: 400,
-        quantity: 7,
-    },
-    {
-        name: 'Захват',
-        price: 1200,
-        quantity: 2,
-    },
+const products = [
+  {
+    name: 'Радар',
+    price: 1300,
+    quantity: 4,
+  },
+  {
+    name: 'Сканер',
+    price: 2700,
+    quantity: 3,
+  },
+  {
+    name: 'Дроид',
+    price: 400,
+    quantity: 7,
+  },
+  {
+    name: 'Захват',
+    price: 1200,
+    quantity: 2,
+  },
 ];
 
 function calculateTotalPrice(arr, productName) {
-    let totalPrice = 0;
+  let totalPrice = 0;
 
-    for (let obj of arr) {
-        const valuesMassive = Object.values(obj); // ПЕРЕДАЛ ЗНАЧЕНИЕ В ПЕРЕМЕННУЮ
+  for (let obj of arr) {
+    const valuesMassive = Object.values(obj); // ПЕРЕДАЛ ЗНАЧЕНИЕ В ПЕРЕМЕННУЮ
 
-        if (valuesMassive.includes(productName)) {
-            totalPrice = obj['price'] * obj['quantity'];
+    if (valuesMassive.includes(productName)) {
+      totalPrice = obj['price'] * obj['quantity'];
 
-            break // ДОБАВИЛ 'BREAK'
-        }
+      break; // ДОБАВИЛ 'BREAK'
     }
+  }
 
-    return totalPrice;
+  return totalPrice;
 }
 
 console.log(calculateTotalPrice(products, 'Радар')); // 5200
---
+++ goit-js-hw-04/js/task-2.js
@@ -0,0 +1,79 @@
+////////////////============== 1 МЕТОД
+
+
+// const inventory = {
+//     items: ['Монорельса', 'Фильтр'],
+//     add(itemName) {
+//         console.log(`Adding ${itemName} to inventory`);
+
+//         this.items.push(itemName);
+//     },
+//     remove(itemName) {
+//         console.log(`Removing ${itemName} from inventory`);
+
+//         this.items = this.items.filter(item => item !== itemName);
+//     },
+// };
+
+// const invokeInventoryAction = function (itemName, action) {
+//     console.log(`Invoking action on ${itemName}`);
+//     action.call(inventory, itemName); // ДОБАВИЛ МЕТОД ФУНКЦИИ "call" для того чтобы передать контекст объекта inventory
+// };
+
+// invokeInventoryAction('Аптечка', inventory.add);
+// // Invoking action on Аптечка
+// // Adding Аптечка to inventory
+
+// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+// invokeInventoryAction('Фильтр', inventory.remove);
+// // Invoking action on Фильтр
+// // Removing Фильтр from inventory
+
+// console.log(inventory.items); // ['Монорельса', 'Аптечка']
+
+
+
+
+
+/////////////////////================== 2 МЕТОД забайндил и вызвал к в контексте объекта "inventory"
+
+
+function add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+};
+
+function remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+};
+
+const inventory = {
+    items: ['Монорельса', 'Фильтр'],
+};
+
+const addFunction = add.bind(inventory)
+const removeFunction = remove.bind(inventory)
+
+inventory.add = addFunction;
+inventory.remove = removeFunction;
+
+const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
+};
+
+invokeInventoryAction('Аптечка', inventory.add);
+// Invoking action on Аптечка
+// Adding Аптечка to inventory
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryAction('Фильтр', inventory.remove);
+// Invoking action on Фильтр
+// Removing Фильтр from inventory
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
